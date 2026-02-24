4495e6955fbc83620dda1d079a0f44435fab0aec
Diana <Diana@DIANA-CAROLINA->
2026-02-24 10:55:24 -0400

    Agregar archivos de proyecto.

---
+++ Ventana1/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Ventana1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(66, 46);
+            button1.TabIndex = 0;
+            button1.Text = "Cuentas";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(84, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(76, 46);
+            button2.TabIndex = 1;
+            button2.Text = "Imprimir";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(166, 14);
+            button3.Name = "button3";
+            button3.Size = new Size(90, 45);
+            button3.TabIndex = 2;
+            button3.Text = "Cerrar caja";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(262, 13);
+            button4.Name = "button4";
+            button4.Size = new Size(104, 45);
+            button4.TabIndex = 3;
+            button4.Text = "Ret/Ing Caja";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(781, 12);
+            button5.Name = "button5";
+            button5.Size = new Size(71, 44);
+            button5.TabIndex = 4;
+            button5.Text = "notas";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(672, 12);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 44);
+            button6.TabIndex = 5;
+            button6.Text = "Productos";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(572, 12);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 45);
+            button7.TabIndex = 6;
+            button7.Text = "Cliente";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(472, 12);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 46);
+            button8.TabIndex = 7;
+            button8.Text = "Ver Caja";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(372, 12);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 45);
+            button9.TabIndex = 8;
+            button9.Text = "Cotizaciones";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(1117, 14);
+            button10.Name = "button10";
+            button10.Size = new Size(91, 44);
+            button10.TabIndex = 9;
+            button10.Text = "Dinorah";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(858, 13);
+            button11.Name = "button11";
+            button11.Size = new Size(80, 44);
+            button11.TabIndex = 10;
+            button11.Text = "Des";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(1033, 14);
+            button12.Name = "button12";
+            button12.Size = new Size(78, 44);
+            button12.TabIndex = 11;
+            button12.Text = "Salir";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.ActiveBorder;
+            label1.Location = new Point(2, 5);
+            label1.Name = "label1";
+            label1.Size = new Size(1212, 66);
+            label1.TabIndex = 12;
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ActiveBorder;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = SystemColors.ActiveCaptionText;
+            textBox1.Location = new Point(944, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(87, 18);
+            textBox1.TabIndex = 13;
+            textBox1.Text = "Caja: Caja #1";
+            // 
+            // label2
+            // 
+            label2.BackColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(2, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(936, 24);
+            label2.TabIndex = 14;
+            label2.Text = "Buscar por Codigo";
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Red;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(2, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(936, 32);
+            label3.TabIndex = 15;
+            label3.Text = "Total: $ 0.00                                                                                                                                                       Cant: 0";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.BackColor = SystemColors.ActiveBorder;
+            label4.Location = new Point(982, 82);
+            label4.Name = "label4";
+            label4.Size = new Size(226, 273);
+            label4.TabIndex = 16;
+            // 
+            // label5
+            // 
+            label5.BackColor = SystemColors.ActiveBorder;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(1065, 92);
+            label5.Name = "label5";
+            label5.Size = new Size(68, 25);
+            label5.TabIndex = 17;
+            label5.Text = "Cliente";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.Location = new Point(995, 126);
+            label6.Name = "label6";
+            label6.Size = new Size(201, 22);
+            label6.TabIndex = 18;
+            label6.Text = "Buscar Cliente";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1220, 450);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ Ventana1/Form1.cs
@@ -0,0 +1,82 @@
+namespace Ventana1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cliente_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
